105e25b1a7fa4619a9a6048b4beb5c1bd4e50dbf
Sebastian Zillessen <sebastian@spitzeljagd.de>
2013-10-15 16:30:32 +0200

    included map caching

---
+++ www/libs/map.js
@@ -0,0 +1,64 @@
+'use strict';
+
+(function (window, emr, L) {
+    var StorageTileLayer = L.TileLayer.extend({
+        log: function (text) {
+            if (this.options.log) this.options.log(text);
+        },
+        _imageToDataUri: function (image) {
+            var canvas = window.document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+
+            var context = canvas.getContext('2d');
+            context.drawImage(image, 0, 0);
+
+            return canvas.toDataURL('image/png');
+        },
+
+        _tileOnLoadWithCache: function () {
+            var storage = this._layer.options.storage;
+            if (storage) {
+                storage.add(this._storageKey, this._layer._imageToDataUri(this));
+            }
+            L.TileLayer.prototype._tileOnLoad.apply(this, arguments);
+        },
+
+        _setUpTile: function (tile, key, value, cache) {
+            tile._layer = this;
+            if (cache) {
+                tile._storageKey = key;
+                tile.onload = this._tileOnLoadWithCache;
+                tile.crossOrigin = 'Anonymous';
+            } else {
+                tile.onload = this._tileOnLoad;
+            }
+            tile.onerror = this._tileOnError;
+            tile.src = value;
+        },
+
+        _loadTile: function (tile, tilePoint) {
+            this.log("_LoadTile");
+            this._adjustTilePoint(tilePoint);
+            var key = tilePoint.z + ',' + tilePoint.y + ',' + tilePoint.x;
+
+            var self = this;
+            if (this.options.storage) {
+                this.options.storage.get(key, function (value) {
+                    if (value) {
+                        self._setUpTile(tile, key, value, false);
+                    } else {
+                        self._setUpTile(tile, key, self.getTileUrl(tilePoint), true);
+                    }
+                }, function () {
+                    self._setUpTile(tile, key, self.getTileUrl(tilePoint), true);
+                });
+            } else {
+                self._setUpTile(tile, key, self.getTileUrl(tilePoint), false);
+            }
+        }
+    });
+
+    window.StorageTileLayer = StorageTileLayer;
+})(window, window.offlineMaps.eventManager, L);
+
--- www/mapCache.js
+++ www/mapCache.js
@@ -22,7 +22,9 @@ var localMapCache = function () {
 
 
     function log(text) {
-        console.log("MapCachePlugin: " + JSON.stringify(text, censor(text)));
+        text = JSON.stringify(text, censor(text));
+        console.log("MapCachePlugin: " + text);
+        $("#log").prepend("<li>" + text + "</li>");
     }
 
     function insertCss(filename) {
@@ -42,127 +44,63 @@ var localMapCache = function () {
             return "Element '" + id + "' could not be founded";
         }
         insertCss("css/leaflet.css");
-
-        window.requestFileSystem(
-            LocalFileSystem.PERSISTENT, 0,
-            function (fs) { //success
-                FILESYSTEM = fs; //set global - sloppy, I know
-                addTileLayer();
-            },
-            function () {
-                alert("Failure accessing filesystem!");
-            } //filesystem failure
-        );
-
-        MAP = L
-            .map(id, {
-                center: [51.505, -0.09],
-                zoom: 13,
-                maxZoom: 14,
-                minZoom: 3,
-                detectRetina: true
+        $.getScript("libs/event.js").done(function () {
+            window.offlineMaps.eventManager
+                .on('storageLoaded', function (storage) {
+                    log("storage is:");
+                    log(storage);
+                    initializeMap(id, storage);
+                });
+            $.getScript("libs/storage.js").done(function () {
+                $.getScript("libs/map.js").done(function () {
+                    log("loaded libs");
+                    window.offlineMaps.eventManager.fire('storageLoad');
+                });
             });
+        });
+        return true;
+    };
 
+    function initializeMap(selector, storage) {
+        log("Map Init");
+        MAP = L.map(selector, {
+            center: [52.521809, 13.412848],
+            zoom: 13,
+            maxZoom: 13,
+            minZoom: 13,
+            detectRetina: true,
+            zoomControl: false
+        });
+        log("adding tile Layer now");
+        log(MAP);
+        log(window.offlineMaps);
+        log(window.offlineMaps.storage);
+        log(StorageTileLayer);
+        new StorageTileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {storage: storage, log: log}).addTo(MAP);
+        log("After storage init");
         MAP
             .locate({watch: true, setView: true, maxZoom: 18})
-            .on('locationfound', function (e) {
-                console.log("Location found");
-                localMapCache.location = e.latlng;
-                addTileLayer();
-            })
+            .on('locationfound', locationFound)
             .on('locationerror', function (e) {
                 log(e);
                 log("LOCATION ERROR");
             });
-
-
-        /*L.tileLayer('http://{s}.tile.cloudmade.com/9259e57a25e3434880e56895ba098a24/{styleId}/256/{z}/{x}/{y}.png', {
-         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-         styleId: 105345,
-         maxZoom: 18
-         }).addTo(MAP);*/
-        return true;
-    };
-
-    function addTileLayer() {
-        if (localMapCache.location && FILESYSTEM) {
-            var layer = L
-                .tileLayer(tilePath(FILESYSTEM), {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                })
-                .addTo(MAP)
-                .on('loading', function (e) {
-                    log("Tile layer is loading tiles now.");
-                })
-                .on('tileload', function (e) {
-                    fileUtils.bulkDownload(FILESYSTEM, [getRemotePath(e.url)], "tiles");
-                });
-
-            var tiles = tileUtils.pyramid("", localMapCache.location.lat, localMapCache.location.lng);
-            /*fileUtils.bulkDownload(FILESYSTEM, tiles, "tiles", function () {
-             log("BulkDownload completed");
-             layer.redraw();
-             });*/
-            log("AddTileLayer included");
-        }
-
-    }
-
-    function getRemotePath(localPath) {
-        // eg: "file:///storage/emulated/0/tiles/14/8802/5375.png"
-        //                                       {z}{x}  {y}
-        var pattern = /.*\/(\d+)\/(\d+)\/(\d+)\.png/;
-        pattern.exec(localPath);
-        var z = parseInt(RegExp.$1);
-        var x = parseInt(RegExp.$2);
-        var y = parseInt(RegExp.$3);
-        if (isNaN(z) || isNaN(x) || isNaN(y)) {
-            log("local Path " + localPath + " could not be matched to pattern.");
-            return;
-        }
-        else {
-            var url = "http://tile.osm.org/{z}/{x}/{y}.png";
-            url = url.replace("{z}", z);
-            url = url.replace("{x}", x);
-            url = url.replace("{y}", y);
-            log("returning remote url '" + url + "' for localPath '" + localPath + "'");
-            return url;
-        }
-    }
-
-    //http://{s}.tile.osm.org/{z}/{x}/{y}.png
-    function tilePath(fileSystem) {
-        //return "http://tile.osm.org/{z}/{x}/{y}.png";
-        var rootDir = fileSystem.root.fullPath;
-        if (rootDir[rootDir.length - 1] != '/') {
-            rootDir += '/';
-        }
-        var r = rootDir + 'tiles/{z}/{x}/{y}.png';
-        log("Tile Path is :" + r);
-        return r;
-    }
-
-    function updatePosition(pos) {
-        if (MAP === null) {
-            log("Map not initialized");
-            return false;
-        }
-        log("updatePosition " + JSON.stringify(pos));
-        if (pos.coords.latitude && pos.coords.longitude) {
-            log("Set position to " + pos.coords.latitude + "|" + pos.coords.longitude);
-            //MAP.setView([pos.coords.latitude, pos.coords.longitude], 13);
-        }
+        log("adding tile Layer: done");
     };
 
 
-    function getTile() {
-        log("get Tile");
+    function locationFound(loc) {
+        console.log("Location found");
+        localMapCache.location = e.latlng;
+        alert("Location found");
     };
+
+
     return {
-        'getTile': getTile,
-        'initialize': init,
-        'updatePosition': updatePosition
+        'initialize': init
     };
-}();
+}
+
+    ();
 
 module.exports = localMapCache;
